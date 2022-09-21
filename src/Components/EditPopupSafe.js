32f4979f6c315edcc015a08e636d1c3939ba6f3c
DivijaBabu <divijababu@piktorlabs.com>
2022-09-22 00:52:23 +0530

    divija good night

--- src/Components/EditPopupSafe.js
+++ src/Components/EditPopupSafe.js
@@ -1,135 +1,123 @@
-import Iconsafe from './icon_image.png';
-import './createform.css';
-import Popup from 'reactjs-popup'
+import shield from "../assets/shield-safe.png";
+import addImage from "../assets/icon_add.png";
 import React from 'react';
-import Edit from './edit-xxl.png';
 import { useState,useEffect } from 'react';
-import { updateSafe } from '../features/Users';
+import { updateSafe } from '../ReduxFolder/Actions';
 import { useDispatch } from 'react-redux';
 
 export default function EditPop(props) {
 
     const [id, setId] = useState("");
-    const [name, setName] = useState("");
-    // const [owner, setOwner] = useState("");
-    const[username,setUsername]=useState("");
+    const [safeName, setSafeName] = useState("");
+    const [owner, setOwner] = useState("");
     const [type, setType] = useState("");
     const [description, setDescription] = useState("");
   
-    useEffect(() => {
-      setId(props.id);
-      setName(props.name);
-      setUsername(props.owner);
-      setType(props.type);
-      setDescription(props.description);
-    },[props.id,props.name,props.owner,props.type,props.description]);
+    // useEffect(() => {
+    //   setId(props.id);
+    //   setName(props.name);
+    //   setUsername(props.owner);
+    //   setType(props.type);
+    //   setDescription(props.description);
+    // },[props.id,props.name,props.owner,props.type,props.description]);
 
-    const dispatch=useDispatch();
+    const updateDispatch=useDispatch();
 
   return (
-    <div>
-          <Popup trigger={ <img src={Edit} className="edit_button" alt="edit"></img>}>
-          <div className="popup">
-      <div className="popup-inner">
-        <h2>Create Safe</h2>
-        <div className="popup-content">
-          <img src={shield} alt="shield" />
-          <p>
-            A Safe is a logical unit to store the secrets. All the safes are
-            created within Vault. You can control access only at the safe level.
-            As a vault administrator you can manage safes but cannot view the
-            content of the safe.
-          </p>
-        </div>
-        <div className="input_content">
-          <p>Safe Name</p>
-          <input
-            id="inputvalue"
-            type="text"
-            placeholder="name"
-            value={safeName}
-            onChange={(event) => {
-              setSafeName(event.target.value);
-            }}
-          />
-          <p>Owner</p>
-          <input
-            id="inputvalue"
-            type="text"
-            placeholder="owner"
-            value={owner}
-            onChange={(event) => {
-              setOwner(event.target.value);
-            }}
-          />
-          <p>Type</p>
-          <select
-            name="cars"
-            id="dropdown"
-            value={type}
-            onChange={(event) => {
-              setType(event.target.value);
+    <div className="popup">
+    <div className="popup-inner">
+      <h2>Create Safe</h2>
+      <div className="popup-content">
+        <img src={shield} alt="shield" />
+        <p>
+          A Safe is a logical unit to store the secrets. All the safes are
+          created within Vault. You can control access only at the safe level.
+          As a vault administrator you can manage safes but cannot view the
+          content of the safe.
+        </p>
+      </div>
+      <div className="input_content">
+        <p>Safe Name</p>
+        <input
+          id="inputvalue"
+          type="text"
+          placeholder="name"
+          value={safeName}
+          onChange={(event) => {
+            setSafeName(event.target.value);
+          }}
+        />
+        <p>Owner</p>
+        <input
+          id="inputvalue"
+          type="text"
+          placeholder="owner"
+          value={owner}
+          onChange={(event) => {
+            setOwner(event.target.value);
+          }}
+        />
+        <p>Type</p>
+        <select
+          name="cars"
+          id="dropdown"
+          value={type}
+          onChange={(event) => {
+            setType(event.target.value);
+          }}
+        >
+          <option value="select">Select</option>
+          <option value="personal">Personal</option>
+          <option value="others">others</option>
+        </select>
+        <p>Description</p>
+        <input
+          id="description"
+          type="text"
+          placeholder="add a description"
+          value={description}
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+        />
+        <p>Please add a minimum of 10 characters</p>
+      </div>
+      <div id="popup_button">
+        <button id="popup_button_cancel" onClick={() => props.close()}>
+          cancel
+        </button>
+        {(safeName.length <= 10 ||
+          owner.length <= 10 ||
+          description.length <= 10) && (
+          <button
+            type="submit"
+            id="popup_button_button"
+            onClick={() => {
+              props.close();
             }}
           >
-            <option value="select">Select</option>
-            <option value="personal">Personal</option>
-            <option value="others">others</option>
-          </select>
-          <p>Description</p>
-          <input
-            id="description"
-            type="text"
-            placeholder="add a description"
-            value={description}
-            onChange={(event) => {
-              setDescription(event.target.value);
-            }}
-          />
-          <p>Please add a minimum of 10 characters</p>
-        </div>
-        <div id="popup_button">
-          <button id="popup_button_cancel" onClick={() => props.close()}>
-            cancel
+            Update
           </button>
-          {(safeName.length <= 10 ||
-            owner.length <= 10 ||
-            description.length <= 10) && (
+        )}
+        {safeName.length > 10 &&
+          owner.length > 10 &&
+          description.length > 10 && (
+            
             <button
               type="submit"
-              id="popup_button_button"
+              id="popup_button_rose"
               onClick={() => {
+                updateDispatch(updateSafe({ id: id, safeName, owner, description }));
                 props.close();
               }}
-            >
-              <img src={addImage} alt="addimage" />
-              Create
+            >      
+              update
             </button>
           )}
-     
-          {safeName.length > 10 &&
-            owner.length > 10 &&
-            description.length > 10 && (
-              
-              <button
-                type="submit"
-                id="popup_button_rose"
-                onClick={() => {
-                  dispatch(addSafe({ id: id, safeName, owner, description }));
-                  props.close();
-                }}
-              >
-                    
-                <img src={addImage} alt="addimage"/>
-                Create
-              </button>
-            )}
-        </div>
       </div>
     </div>
-   </Popup> 
-
-    </div>
-  )
+  </div>
+);
 }
 
 
--- src/Components/safes.js
+++ src/Components/safes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import safeImage from "../assets/shieldimage.png";
 import search from "../assets/icon_search.png";
 import computer from "../assets/computer.png";
@@ -6,75 +6,125 @@ import create from "../assets/all-safes-create.png";
 import folder from "../assets/add-folder.png";
 import locker from "../assets/locker.png";
 import add from "../assets/icon_add.png";
-import Popup from 'reactjs-popup';
+import Popup from "reactjs-popup";
 import PopupSafe from "../Components/popup_safe";
-import Addfolder from './addfolder';
-import ListSafe from "../assets/shield-safe.png"
-import editImage from "../assets/editimage.png"
-import deleteImage from "../assets/deleteimage.png"
-import {useSelector,useDispatch } from "react-redux";
-import { deleteSafe } from '../ReduxFolder/Actions';
+import Addfolder from "./addfolder";
+import ListSafe from "../assets/shield-safe.png";
+import editImage from "../assets/editimage.png";
+import deleteImage from "../assets/deleteimage.png";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteSafe } from "../ReduxFolder/Actions";
+import EditPopup from "../Components/EditPopupSafe";
 export default function Safes() {
-  const [blankpage, setBlankpage] = useState('addbutton');
+  const [blankpage, setBlankpage] = useState("addbutton");
   const update_blank = () => {
-    setBlankpage('button_update')
-  }
-  const deletedispatch=useDispatch();
+    setBlankpage("button_update");
+  };
+  const deletedispatch = useDispatch();
 
   // const curId = useSelector((state) => state.users.curId);
   // const dispatching=useDispatch();
-  const userList=useSelector((state)=>state.users.value);
+  const userList = useSelector((state) => state.users.value);
   // const secretList = useSelector((state) => state.users.value);
   return (
     <div id="bodycontainer_safe">
-      <div id='safes'>
+      <div id="safes">
         <div id="allsafes">
           <div id="safehead">
-            <div id='safecard'>
+            <div id="safecard">
               <div id="text">All Safes</div>
-              <div id='zero'>(0)</div>
+              <div id="zero">(0)</div>
             </div>
-            <div id='searchbar'>
+            <div id="searchbar">
               <img src={search} alt="search" />
-              <input type="text" placeholder='Search' />
+              <input type="text" placeholder="Search" />
             </div>
           </div>
-          <div id='computer'>
-          {userList.map((user)=>
-        {
-            return(
-               <div id="listcontainer">
-                <div id="shieldimage">
-                <img src={ListSafe} alt="safe"/></div>
-                 <div id="nameandowner">
-                    <div>{user.safeName}</div>
-                    <div>{ user.owner}</div>
+          <div id="computer">
+            {userList.length <= 0 && (
+              <div>
+                <img src={computer} alt="search" />
+                <p>Create a Safe and get started!</p>
+                <Popup
+                  trigger={
+                    <img
+                      id="create-image"
+                      src={create}
+                      alt="create"
+                      onClick={update_blank}
+                    />
+                  }
+                  modal
+                  nested
+                >
+                  {(close) => <PopupSafe close={close} />}
+                </Popup>
+              </div>
+            )}
+            {userList.map((user) => {
+              return (
+                <div>
+                  <div id="listcontainer">
+                    <div id="shieldimage">
+                      <img src={ListSafe} alt="safe" />
+                    </div>
+                    <div id="nameandowner">
+                      <div>{user.safeName}</div>
+                      <div>{user.owner}</div>
+                    </div>
+                    <div id="editanddeletebutton">
+                      <Popup
+                        trigger={<img src={editImage} alt="edit" />}
+                        modal
+                        nested
+                      >
+                        {(close) => (
+                          <EditPopup
+                            id={user.id}
+                            safeName={user.safeName}
+                            owner={user.owner}
+                            type={user.type}
+                            description={user.description}
+                          />
+                        )}
+                      </Popup>
+                      <img
+                        src={deleteImage}
+                        alt="delete"
+                        onClick={() => {
+                          deletedispatch(deleteSafe({ id: user.id }));
+                        }}
+                      />
+                    </div>
                   </div>
-                  <div id="editanddeletebutton">
-                    <img src={editImage} alt="edit"/>
-                    <img src={deleteImage} alt="delete" onClick={()=>{deletedispatch(deleteSafe({id:user.id}));}}/>
-                  </div>
-               </div>
-            )
-        })}
-            <img src={computer} alt="search" />
-            <p>Create a Safe and get started!</p>
-            <Popup
-              trigger={<img id="create-image" src={create} alt="create" onClick={update_blank}/>} modal nested
-            >
-              {(close) => (
-                <PopupSafe close={close} />
-              )}
-            </Popup>
+                </div>
+              );
+            })}
+            {userList.length > 0 && (
+              <div>
+                <Popup
+                  trigger={
+                    <img
+                      id="list-create-image"
+                      src={create}
+                      alt="create"
+                      onClick={update_blank}
+                    />
+                  }
+                  modal
+                  nested
+                >
+                  {(close) => <PopupSafe close={close} />}
+                </Popup>
+              </div>
+            )}
           </div>
         </div>
         <div id="secrets">
           <div id="secretshead">
             <img src={safeImage} alt="shield" />
             <div id="container-secrets">
-              <p id="no-safes">
-                No Safes Created Yet
-              </p>
+              <p id="no-safes">No Safes Created Yet</p>
               <p id="create-a-safe">
                 Create a Safe to see your secrets, folders and permissions here
               </p>
@@ -87,9 +137,7 @@ export default function Safes() {
               </div>
               <div id="add-folders">
                 <Popup trigger={<img src={folder} alt="folder" />} modal nested>
-                  {(close) => (
-                    <Addfolder close={close} />
-                  )}
+                  {(close) => <Addfolder close={close} />}
                 </Popup>
               </div>
             </div>
@@ -98,18 +146,24 @@ export default function Safes() {
                 <p>0 secrets</p>
                 <div id="locker">
                   <img id="locker-image" src={locker} alt="locker" />
-                  <p id="locker-para">Add a Folder and then you’ll be able to add Secrets to view them all here</p>
+                  <p id="locker-para">
+                    Add a Folder and then you’ll be able to add Secrets to view
+                    them all here
+                  </p>
                   <div id="create-secrets">
                     <div id="add-secrets">
-                      <Popup trigger={<button>
-                <img id='add-secrets-image' src={add} alt="add" /> 
-                <p>Add</p>
-                </button> } modal nested>
-                  {(close) => (
-                    <Addfolder close={close} />
-                  )}
-                </Popup>
-                
+                      <Popup
+                        trigger={
+                          <button>
+                            <img id="add-secrets-image" src={add} alt="add" />
+                            <p>Add</p>
+                          </button>
+                        }
+                        modal
+                        nested
+                      >
+                        {(close) => <Addfolder close={close} />}
+                      </Popup>
                     </div>
                   </div>
                 </div>
@@ -119,5 +173,5 @@ export default function Safes() {
         </div>
       </div>
     </div>
-  )
+  );
 }
